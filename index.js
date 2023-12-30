74dbb70769ce589168cfa7de2fa08aba6bf18375
helloTiKi <1440568001@qq.com>
2023-12-30 13:19:14 +0800

    1

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-import lodash from "lodash"
 import genshin from "./game/genshin/genshin.js"
 import user from "./user/user.js";
 import { StarRail } from "./game/StarRail/StarRail.js";
 import { bh3 } from "./game/bh3/bh3.js";
 
-
+// 关闭https证书验证，用于开发环境
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '1';
 
-
-let _genshin = bh3.initByUserName('17040490122')
-
-if (!_genshin) {
-    let use = new user();
-    let data = await use.loginByPassWord('17040490122', 'whw123..')
-} else {
-    let userdata = await _genshin.getUserGameRoles()
-    console.log(userdata)
-    userdata = await _genshin.sign(userdata[0].game_uid)
-    console.log(userdata)
-}
+export { genshin, StarRail, bh3, user }
